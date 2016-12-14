65eddc03b48b47e8563abb79268a286c18931ff7
Ajit Chelat <ajit.chelat@citrix.com>
2016-12-14 10:42:27 +0530

    Updated theme

--- theme_multi/js/custom.js
+++ theme_multi/js/custom.js
@@ -22,6 +22,19 @@ $(document).ready(function(){
 			ul.hide(300);
 		}
 	})
+	
+	$(".expcli").click(function(){
+	var ul = $(this).parent().next();
+	if ( ul.css('display') == 'none' ){
+		$(this).children('.fa').removeClass('fa-plus-square-o').addClass('fa-minus-square-o');
+		ul.show(300);
+	}
+	else{	
+		$(this).children('.fa').removeClass('fa-minus-square-o').addClass('fa-plus-square-o');
+		ul.hide(300);
+	}
+	})
+	
 	$(".expul >i.fa").click(function(){
 		var lis = $(this).parent().parent().siblings(".toctree-l1 ");
 		if ( $(this).hasClass('fa-plus-square-o')){
